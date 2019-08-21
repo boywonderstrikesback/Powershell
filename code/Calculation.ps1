e2c48de5c5c638123529a8f3d90037773140899f
Mike Ross <34175985+boywonderstrikesback@users.noreply.github.com>
2019-08-21 09:08:46 -0400

    Add files via upload

---
+++ code/Calculation.ps1
@@ -0,0 +1,5 @@
+﻿$r=2
+$pi=[math]::pi
+$r2=[math]::pow( $r, 2 )
+$Area = $pi*$r2
+$Area
---
+++ code/Parameter.ps1
@@ -0,0 +1,9 @@
+﻿#Parameter of a Square
+$Result = 5+5+5+5
+[pscustomobject]@{
+    Side1 = 5
+    Side2 = 5
+    Side3 = 5
+    Side4 = 5
+    Perimeter = $Result
+}
